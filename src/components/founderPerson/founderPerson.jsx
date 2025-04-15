d7c7ed309613e05ca4e589917f20914977793e9e
Huyz109 <phamqhuy2001@gmail.com>
2025-04-15 13:45:13 +0700

    feat: update member image

--- src/components/founderPerson/founderPerson.jsx
+++ src/components/founderPerson/founderPerson.jsx
@@ -2,9 +2,12 @@ import * as React from "react";
 import "./founderPerson.scss";
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
-import founder1Danh from "../../assets/image/founders/danh.png";
-import founder2 from "../../assets/image/founders/teacher_1.jpg";
-import founder3 from "../../assets/image/founders/teacher_2.jpg";
+import HoTrongThanh from "../../assets/image/founders/HoTrongThanh.jpg";
+import NguyenHaiMinh from "../../assets/image/founders/NguyenHaiMinh.jpg";
+import NguyenQuangThinh from "../../assets/image/founders/NguyenQuangThinh.jpg";
+import PhungBaTruongGiang from "../../assets/image/founders/PhungBaTruongGiang.jpg";
+import PhamMinhHoang from "../../assets/image/founders/PhamMinhHoang.jpg";
+import LogoSparc from "../../assets/image/logo_sparc.png";
 
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { useTranslation } from "react-i18next";
@@ -51,7 +54,8 @@ const FounderPersonSection = () => {
       name: "Akira Wada ",
       posistion: "Project Manager",
       quote: t("page.founderPerson.members.toni.role"),
-    },    {
+    },
+    {
       id: 3,
       name: "Tsukasa Okabe",
       posistion: "Project Manager",
@@ -68,33 +72,38 @@ const FounderPersonSection = () => {
       name: "Truong-Giang Phung",
       posistion: "Software Architect, Graphic Engineer",
       quote: t("page.founderPerson.members.toni.role"),
+      img: PhungBaTruongGiang,
     },
     {
       id: 6,
       name: "Hai-Minh Nguyen ",
       posistion: "Embedded Software Developer",
       quote: t("page.founderPerson.members.toni.role"),
+      img: NguyenHaiMinh,
     },
     {
       id: 7,
       name: "Minh-Hoang Nguyen  ",
       posistion: "Artificial Intelligence Engineer",
       quote: t("page.founderPerson.members.toni.role"),
+      img: PhamMinhHoang,
     },
     {
       id: 8,
       name: "Trong-Thanh Ho  ",
       posistion: "Artificial Intelligence Engineer",
       quote: t("page.founderPerson.members.toni.role"),
+      img: HoTrongThanh,
     },
     {
       id: 9,
       name: "Thinh-Quang Nguyen  ",
       posistion: "Artificial Intelligence Engineer",
       quote: t("page.founderPerson.members.toni.role"),
+      img: NguyenQuangThinh,
     },
   ];
-  
+
   const handleClickCard = (data) => {
     setShow(false);
     setinfoTeacher(data);
@@ -106,20 +115,31 @@ const FounderPersonSection = () => {
       <div className="container-team">
         <div className="header-container">
           <h1>{t("page.founderPerson.title")}</h1>
-          <p>
-            {t("page.founderPerson.description")}
-          </p>
+          <p>{t("page.founderPerson.description")}</p>
         </div>
         <div className="container-team">
-          {data.map((item,index)=>(
-            <div className="team-member" onClick={()=> {handleClickCard(item)}}>
-            {/* <img src={item.img} alt="image-founder-item" className="personal-image"/> */}
-            <div className="team-info">
-              <div className="left">
-                <h3>{item.name}</h3>
-                <p>{item.posistion}</p>
-              </div>
-              {/* <div className="social-icons">
+          {data.map((item, index) => (
+            <div
+              className="team-member"
+              onClick={() => {
+                handleClickCard(item);
+              }}
+            >
+              {item.img ? (
+                <img
+                  src={item.img}
+                  alt="image-founder-item"
+                  className="personal-image"
+                />
+              ) : (
+                <img src={LogoSparc} alt="SPARC" className="logo-image" />
+              )}
+              <div className="team-info">
+                <div className="left">
+                  <h3>{item.name}</h3>
+                  <p>{item.posistion}</p>
+                </div>
+                {/* <div className="social-icons">
                 <a href="#">
                   <i className="fab fa-instagram" />
                 </a>
@@ -127,9 +147,9 @@ const FounderPersonSection = () => {
                   <i className="fab fa-twitter" />
                 </a>
               </div> */}
+              </div>
             </div>
-          </div>
-          ))}          
+          ))}
         </div>
       </div>
       {/* <ModalHandleClickCard
